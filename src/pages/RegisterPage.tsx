d197a2218f04106d2527e1d219f96e54b2165ffa
NicolasFernandez1701 <nicoo.e.fernandez@gmail.com>
2024-06-05 22:45:15 -0300

    Initial Commit

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { RegisterScreen } from '../components';
+import '../../assets/styles/styles.css';
+
+export const RegisterPage = () => {
+  return (
+    <div className="container">
+      <RegisterScreen />
+    </div>
+  )
+}
